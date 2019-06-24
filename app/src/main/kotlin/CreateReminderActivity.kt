f2143781ada9bb0d85b400ed54a38f15f83162e7
Yann Zaoui <yzaoui7@gmail.com>
2019-06-24 16:50:37 -0400

    Add menu button to create reminder screen

--- app/src/main/kotlin/CreateReminderActivity.kt
+++ app/src/main/kotlin/CreateReminderActivity.kt
@@ -3,6 +3,7 @@ package com.bitwiserain.remindme
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_create_reminder.*
 
@@ -13,6 +14,11 @@ class CreateReminderActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_create_reminder, menu)
+        return true
     }
 
     companion object {
