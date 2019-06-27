83a548589f2907d9fa290581f004e0bc89f3a777
Yann Zaoui <yzaoui7@gmail.com>
2019-06-26 20:58:05 -0400

    Replace main content with a fragment

---
+++ app/src/main/kotlin/ReminderListFragment.kt
@@ -0,0 +1,13 @@
+package com.bitwiserain.remindme
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ReminderListFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_reminder_list, container, false)
+    }
+}
