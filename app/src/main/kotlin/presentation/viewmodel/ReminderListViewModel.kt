8ac83c8a0f33dd0e12c5887626c28a2366d97e4e
Yann Zaoui <contact@yannzaoui.com>
2020-07-09 23:42:01 -0400

    Sort displayed reminders chronologically

--- app/src/main/kotlin/presentation/viewmodel/ReminderListViewModel.kt
+++ app/src/main/kotlin/presentation/viewmodel/ReminderListViewModel.kt
@@ -1,6 +1,7 @@
 package com.bitwiserain.remindme.presentation.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bitwiserain.remindme.room.Reminder
@@ -8,7 +9,9 @@ import com.bitwiserain.remindme.room.ReminderRepository
 import kotlinx.coroutines.launch
 
 class ReminderListViewModel internal constructor(private val repo: ReminderRepository) : ViewModel() {
-    val reminders: LiveData<List<Reminder>> = repo.getReminders()
+    val reminders: LiveData<List<Reminder>> = Transformations.map(repo.getReminders()) {
+        it.sortedBy { it.time }
+    }
 
     fun deleteReminder(reminder: Reminder) = viewModelScope.launch {
         repo.deleteReminder(reminder)
