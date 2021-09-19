4254d5248e9c90f9c3df81b2e9bd989f0fef42db
Yann Zaoui <contact@yannzaoui.com>
2021-09-18 21:57:47 -0400

    Convert :app build.gradle to .kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,36 +1,38 @@
-apply plugin: "com.android.application"
-apply plugin: "kotlin-android"
-apply plugin: "kotlin-kapt"
-apply plugin: "de.mannodermaus.android-junit5"
-apply plugin: "androidx.navigation.safeargs.kotlin"
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+apply(plugin = "de.mannodermaus.android-junit5")
+apply(plugin = "androidx.navigation.safeargs.kotlin")
 
 android {
-    compileSdkVersion(30)
+    compileSdk = 30
     defaultConfig {
         applicationId = "com.bitwiserain.remindme"
-        minSdkVersion(23)
-        targetSdkVersion(30)
+        minSdk = 23
+        targetSdk = 30
         versionCode = 3
         versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
         release {
-            minifyEnabled = false
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     sourceSets {
-        main.java.srcDirs += "src/main/kotlin"
-        androidTest.java.srcDirs += "src/androidTest"
-        test.java.srcDirs += "/src/test"
+        getByName("main").java.srcDirs("src/main/kotlin")
+        getByName("androidTest").java.srcDirs("src/androidTest")
+        getByName("test").java.srcDirs("/src/test")
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
     buildFeatures {
         dataBinding = true
@@ -43,21 +45,21 @@ android {
     }
     testOptions {
         unitTests {
-            includeAndroidResources = true
+            isIncludeAndroidResources = true
             all {
-                useJUnitPlatform()
+                it.useJUnitPlatform()
             }
         }
     }
 }
 
 dependencies {
-    def room_version = "2.3.0"
-    def coroutines_version = "1.5.0"
-    def junit5_version = "5.7.2"
-    def lifecycle_version = "2.3.1"
+    val room_version = "2.3.0"
+    val coroutines_version = "1.5.0"
+    val junit5_version = "5.7.2"
+    val lifecycle_version = "2.3.1"
 
-    implementation(fileTree(dir: "libs", include: ["*.jar"]))
+    implementation(fileTree("dir" to "libs", "include" to listOf("*.jar")))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.core:core-ktx:1.6.0")
@@ -69,8 +71,8 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.3.6")
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
-    implementation("androidx.navigation:navigation-fragment-ktx:$navigation_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$navigation_version")
+    implementation("androidx.navigation:navigation-fragment-ktx:${rootProject.extra["navigation_version"]}")
+    implementation("androidx.navigation:navigation-ui-ktx:${rootProject.extra["navigation_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
 
@@ -82,17 +84,22 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core-jvm:4.2.6") // Kotest core JVM assertions
 }
 
-tasks.withType(Test) {
+tasks.withType<Test> {
     testLogging {
-        events "passed", "skipped", "failed"
+        events = setOf(PASSED, SKIPPED, FAILED)
     }
-    afterSuite { desc, result ->
-        if (!desc.parent) {
-            println("\nTest result: ${result.resultType}")
-            println("Test summary: ${result.testCount} tests, " +
-                    "${result.successfulTestCount} succeeded, " +
-                    "${result.failedTestCount} failed, " +
-                    "${result.skippedTestCount} skipped")
+    addTestListener(object : TestListener {
+        override fun beforeSuite(suite: TestDescriptor) {}
+        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+            if (suite.parent == null) {
+                println("\nTest result: ${result.resultType}")
+                println("Test summary: ${result.testCount} tests, " +
+                        "${result.successfulTestCount} succeeded, " +
+                        "${result.failedTestCount} failed, " +
+                        "${result.skippedTestCount} skipped")
+            }
         }
-    }
+        override fun beforeTest(testDescriptor: TestDescriptor) {}
+        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {}
+    })
 }
