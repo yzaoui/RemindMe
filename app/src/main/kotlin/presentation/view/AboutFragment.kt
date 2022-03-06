ccf7e3fe707e5918b642a2dbeeea54ba667ad961
Yann Zaoui <contact@yannzaoui.com>
2022-03-06 01:32:55 -0500

    Replace fragment_about.xml with AboutScreen

--- app/src/main/kotlin/presentation/view/AboutFragment.kt
+++ app/src/main/kotlin/presentation/view/AboutFragment.kt
@@ -4,19 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.bitwiserain.remindme.BuildConfig
 import com.bitwiserain.remindme.R
-import com.bitwiserain.remindme.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
-    private lateinit var binding: FragmentAboutBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentAboutBinding.inflate(inflater, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
+            setContent { AboutScreen() }
+        }
+}
 
-        binding.aboutVersion.text = getString(R.string.about_version, BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE)
-
-        return binding.root
-    }
+@Preview(showSystemUi = true)
+@Composable
+fun AboutScreen() {
+    Text(
+        text = stringResource(R.string.about_version, BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp),
+        textAlign = TextAlign.Center
+    )
 }
